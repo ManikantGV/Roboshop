8bc5b0e413075ab75be407f4ff29bffc89a0a19a
Manikanta <manikanta.guntika@gmail.com>
2023-10-14 16:04:13 +0530

    Frontend and robshopconf

--- frontend.sh
+++ frontend.sh
@@ -7,8 +7,10 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
+c
 systemctl restart nginx
 
