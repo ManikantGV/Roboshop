791c11e2196c16d576f6be9ca8316c01b943ce93
Manikanta <manikanta.guntika@gmail.com>
2023-10-14 21:31:19 +0530

    adding the copy roboshop.conf

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,6 @@
 yum install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
@@ -6,7 +8,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-#cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 #some files needs to create
 systemctl enable nginx
 systemctl restart nginx
