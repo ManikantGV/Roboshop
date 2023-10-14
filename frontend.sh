2cce4db62e27537e986b081f3213157295197fd5
Manikanta <manikanta.guntika@gmail.com>
2023-10-14 21:27:53 +0530

    frontend created

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,13 @@
-dnf install nginx -y
-
+yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+#cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+#some files needs to create
+systemctl enable nginx
 systemctl restart nginx
 
