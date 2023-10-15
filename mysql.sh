3b91a55246a5b17e383b7870ffe00e94899c02a7
Manikanta <manikanta.guntika@gmail.com>
2023-10-15 13:30:40 +0530

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ echo -e "\e[36m >>>>>>> installing the  mysql >>>>>>>> \e[0m"
 dnf install mysql-community-server -y
 
 echo -e "\e[36m >>>>>>> copying the default repo file >>>>>>>> \e[0m"
-cp mqsql.conf /etc/yum.repos.d/mysql.repo
+cp mysql.conf /etc/yum.repos.d/mysql.repo
 
 echo -e "\e[36m >>>>>>> Enable and stating the mysql >>>>>>>> \e[0m"
 systemctl enable mysqld
