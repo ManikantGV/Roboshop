95fe8f3e7f6dc01f333c2aa6dcb6e79cbe06912d
Manikanta <manikanta.guntika@gmail.com>
2023-10-15 13:27:54 +0530

    add copy command for cart.service

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,16 @@
+
+echo -e "\e[36m >>>>>>> disabling the default mysql >>>>>>>> \e[0m"
 dnf module disable mysql -y
-cp mqsql.conf /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m >>>>>>> installing the  mysql >>>>>>>> \e[0m"
 dnf install mysql-community-server -y
 
+echo -e "\e[36m >>>>>>> copying the default repo file >>>>>>>> \e[0m"
+cp mqsql.conf /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m >>>>>>> Enable and stating the mysql >>>>>>>> \e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[36m >>>>>>> setting up the roboshop user >>>>>>>> \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
