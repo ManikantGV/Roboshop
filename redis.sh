e2cba69cdaca1eb8c0390777648a9a1c6906e085
Manikanta <manikanta.guntika@gmail.com>
2023-10-21 09:08:18 +0530

    mysql.sh

--- redis.sh
+++ redis.sh
@@ -4,7 +4,7 @@ dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 echo -e "\e[36m >>>>>>> enabling the redis servier >>>>>>>> \e[0m"
 dnf module enable redis:remi-6.2 -y
 
-echo -e "\e[36m >>>>>>> Enabling the redis server >>>>>>>> \e[0m"
+echo -e "\e[36m >>>>>>> Installing the redis server >>>>>>>> \e[0m"
 dnf install redis -y
 
 echo -e "\e[36m >>>>>>> changing the ip address 0.0.0.0 in redis.conf >>>>>>>> \e[0m"
