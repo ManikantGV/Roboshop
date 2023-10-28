d7b0afa6dbb35d389e75aca2335a0d0fd295273d
Manikanta <manikanta.guntika@gmail.com>
2023-10-29 01:27:58 +0530

    adding the if condation for log file -- common.sh changes

--- common.sh
+++ common.sh
@@ -1,8 +1,16 @@
 
-logfile=/temp/roboshop.log
+log_file=/temp/roboshop.log
 #rm -f roboshop.log
+if [ -z $log_file ]; then
+  rm -f $log_file
+  mkdir $log_file
+else
+  mkdir $log_file
+fi
+
+
 
 function print_head() {
     echo -e "\e[36m>>>>>>>> $1 >>>>>>>>>>>>>>>\e[0m"
-    echo -e "\e[36m>>>>>>>> $1 >>>>>>>>>>>>>>>\e[0m" &>>$logfile
+    echo -e "\e[36m>>>>>>>> $1 >>>>>>>>>>>>>>>\e[0m" &>>log_file
 }
