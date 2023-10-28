09165fb3e5a3d43305a2919f1be36eeb7321fc39
Manikanta <manikanta.guntika@gmail.com>
2023-10-29 01:49:56 +0530

    adding the if condation for log file -- common.sh changes

--- common.sh
+++ common.sh
@@ -10,3 +10,12 @@ function print_head() {
     echo -e "\e[36m>>>>>>>> $1 >>>>>>>>>>>>>>>\e[0m"
     echo -e "\e[36m>>>>>>>> $1 >>>>>>>>>>>>>>>\e[0m" &>>$log_file
 }
+
+function stat_check() {
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[31m >>>>>> SUCCESS >>>>>>>> \e[0m"
+    else
+      echo -e "\e[32m >>>>>> SUCCESS >>>>>>>> \e[0m"
+      echo "Refer the log file /tmp/roboshop.log for more information"
+    fi
+}
--- frontend.sh
+++ frontend.sh
@@ -1,38 +1,38 @@
 source common.sh
 
-#echo -e "\e[36m >>>>>>> installing the nginx repo >>>>>>>> \e[0m"
 print_head "installing the nginx repo"
 yum install nginx -y &>>$log_file
+stat_check $?
 
-
-#echo -e "\e[36m >>>>>>> copying roboshop config file >>>>>>>> \e[0m"
 print_head "copying roboshop config file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+stat_check $?
 
-#echo -e "\e[36m >>>>>>> enable and start nginx   >>>>>>>> \e[0m"
 print_head "enable and start nginx"
 systemctl enable nginx &>>$log_file
+stat_check $?
 systemctl start nginx &>>$log_file
+stat_check $?
 
-#echo -e "\e[36m >>>>>>> removing the nginx config >>>>>>>> \e[0m"
 print_head "removing the nginx config"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+stat_check $?
 
-#echo -e "\e[36m >>>>>>> Downloading the frontend app zip >>>>>>>> \e[0m"
 print_head "Downloading the frontend app zip"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+stat_check $?
 
-#echo -e "\e[36m >>>>>>> Changing the directory >>>>>>>> \e[0m"
 print_head "Changing the directory"
 cd /usr/share/nginx/html &>>$log_file
+stat_check $?
 
-#echo -e "\e[36m >>>>>>> unziping frontend zip folder >>>>>>>> \e[0m"
 print_head "unziping frontend zip folder"
 unzip /tmp/frontend.zip &>>$log_file
+stat_check $?
 
-#cho -e "\e[36m >>>>>>> enable and restart nginx >>>>>>>> \e[0m"
 print_head "enable and restart nginx"
 #some files needs to create
 systemctl enable nginx &>>$log_file
+stat_check $?
 systemctl restart nginx &>>$log_file
-
+stat_check $?
