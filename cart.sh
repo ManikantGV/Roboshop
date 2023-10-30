8de55fc623721d5580d0be8636ec2cd345fc256e
Manikanta <manikanta.guntika@gmail.com>
2023-10-30 23:12:40 +0530

    adding cart.sh

--- cart.sh
+++ cart.sh
@@ -1,32 +1,12 @@
-echo -e "\e[36m >>>>>>> Configuration nodeJS repos >>>>>>>> \e[0m"
-sudo yum install https://rpm.nodesource.com/pub_18.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
-sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 
-echo -e "\e[36m >>>>>>> Creating the roboshop user >>>>>>>> \e[0m"
-useradd roboshop
+print_head "cart Script Initiating"
 
-echo -e "\e[36m >>>>>>> Creating the app folder >>>>>>>> \e[0m"
-rm -rf /app
-mkdir /app
+component=cart
+nodejs
 
-echo -e "\e[36m >>>>>>> Downloading the app content >>>>>>>> \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-
-echo -e "\e[36m >>>>>>> unzipping the app content >>>>>>>> \e[0m"
-unzip /tmp/cart.zip
-cd /app
-
-echo -e "\e[36m >>>>>>> Install nodeJS dependencies >>>>>>>> \e[0m"
-npm install
-
-echo -e "\e[36m >>>>>>> copying the cart service >>>>>>>> \e[0m"
-cp /root/Roboshop/cart.service /etc/systemd/system/cart.service
-
-echo -e "\e[36m >>>>>>> start Cart service >>>>>>>> \e[0m"
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
 
 
 
--- user.sh
+++ user.sh
@@ -2,7 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-print_head "Catalogue Script Initiating"
+print_head "user Script Initiating"
 
 component=user
 nodejs
