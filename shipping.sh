0d854bfca32315515571065834a26aab5b0495d0
Manikanta <manikanta.guntika@gmail.com>
2023-10-31 00:11:22 +0530

    adding shipping

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-#password
+#password -- RoboShop@1
 if [ -z "$mysql_root_password" ]; then
     echo mysql password is empty
     exit
